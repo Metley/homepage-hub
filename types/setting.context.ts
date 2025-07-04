5ef7106880cdd05647521b3c51e33ca01bf2978b
Metley <37466817+Metley@users.noreply.github.com>
2025-07-04 03:50:08 -0400

    Added sortPreference to Setting interface

--- types/setting.context.ts
+++ types/setting.context.ts
@@ -2,6 +2,13 @@ export interface Setting {
   theme: string;
   navBar: boolean;
   viewStyle: string;
+  sortPrefrence: SortPrefrence;
 }
 
 export type ThemePreference = "System" | "Dark" | "Light";
+export type SortPrefrence =
+  | "CUSTOM"
+  | "ADDED - ASCENDING"
+  | "ADDED - DESCENDING"
+  | "NAME - ASCENDING"
+  | "NAME - DESCENDING";
